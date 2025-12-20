b6b083e43268d244ec2ecb2afe613183441d2220
ashaytiwari <ashaytiwari2001@gmail.com>
2025-12-21 01:03:55 +0530

    added checks for anonymous commands, striped the user actions, applied for loop for show command

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ tasks = []
 while True:
     user_action = input("Type add, show or exit: ")
 
-    match user_action:
+    match user_action.strip():
         
         case "add":
             todo = input("Enter Task: ")
             tasks.append(todo)
 
         case "show":
-            print(tasks)
+            for item in tasks:
+                print(item)
 
         case "exit":
             break
+        
+        case _:
+            print('Hey you have entered an unknown command')
+        
+print('Bye, see you soon!')
